5f955108b249a73a952be9e83f1fc0a12de9fbf7
MineStar <band1019@naver.com>
2021-12-15 01:44:00 +0900

    # ParticleUtil play 에 location 입력값으로 변경

--- src/main/kotlin/kr/kro/minestar/utility/particle/ParticleUtil.kt
+++ src/main/kotlin/kr/kro/minestar/utility/particle/ParticleUtil.kt
@@ -1,6 +1,5 @@
 package kr.kro.minestar.utility.particle
 
-import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.entity.Player
@@ -9,7 +8,6 @@ import org.bukkit.entity.Player
 class ParticleUtil {
     var particle = Particle.FLAME
     var count = 1
-    var location = Location(Bukkit.getWorlds()[0]!!, 0.0, 0.0, 0.0)
     var offsetX = 0.0
     var offsetY = 0.0
     var offsetZ = 0.0
@@ -21,7 +19,7 @@ class ParticleUtil {
     /**
      * 파티클을 재생합니다.
      */
-    fun play() {
+    fun play(location: Location) {
         if (players.isEmpty()) return location.world.spawnParticle(particle, location, count, offsetX, offsetY, offsetZ, speed, data, force)
         for (p in players) p.spawnParticle(particle, location, count, offsetX, offsetY, offsetZ, speed, force)
     }
