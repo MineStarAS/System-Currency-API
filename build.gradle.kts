8e0f521c8514e699f3e983227d876681c7423bcf
MineStar <band1019@naver.com>
2021-12-19 06:23:53 +0900

    # 컬렉션 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
 group = "kr.kro.minestar"
 version = "1.5.0"
 
+val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\libs")
+//val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.17.1 - vanilla\\plugins")
 
 
 repositories {
@@ -61,8 +63,6 @@ tasks {
             // jar file copy
             copy {
                 from(archiveFile)
-                val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\libs")
-//                val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.17.1 - vanilla\\plugins")
                 into(if (File(plugins, archiveFileName.get()).exists()) plugins else plugins)
             }
         }
