c4d7f3134ea2996c2c06a7b3d9c25c48432012ea
MineStar <band1019@naver.com>
2021-12-20 04:50:39 +0900

    # PlaySound 클래스 어드레스 수정

--- src/main/kotlin/kr/kro/minestar/utility/scheduler/objectes/later/RunTitle.kt
+++ src/main/kotlin/kr/kro/minestar/utility/scheduler/objectes/later/RunTitle.kt
@@ -11,18 +11,8 @@ import org.bukkit.scheduler.BukkitTask
 class RunTitle(players: Collection<Player>, val title: String, val subtitle: String, val fadeIn: Int, val stay: Int, val fadeOut: Int, val wait: Int) : RunTask {
     override val runnable: Runnable = Runnable { for (p in players) p.sendTitle(title, subtitle, fadeIn, stay, fadeOut) }
     override val delay: Long = (fadeIn + stay + fadeOut + wait).toLong()
-    private var playSound: PlaySound? = null
 
     override fun run(pl: JavaPlugin, delay: Long): BukkitTask {
-        if (playSound != null) RunPlaySound(playSound!!).run(pl, delay - this.delay)
         return Bukkit.getScheduler().runTaskLater(pl, runnable, delay - this.delay)
-    }
-
-    /**
-     * run 실행 시 출력되는 소리를 설정합니다.
-     */
-    fun setPlaySound(playSound: PlaySound): RunTitle {
-        this.playSound = playSound
-        return this
     }
 }
--- src/main/kotlin/kr/kro/minestar/utility/sound/PlaySound.kt
+++ src/main/kotlin/kr/kro/minestar/utility/sound/PlaySound.kt
@@ -9,24 +9,25 @@ import org.bukkit.entity.Player
  * playSound 를 저장하기 위한 클래스 입니다.
  */
 class PlaySound {
-    var players: Collection<Player> = listOf()
-    var location: Location? = null
     var sound: Sound = Sound.BLOCK_STONE_BREAK
     var soundCategory: SoundCategory = SoundCategory.MASTER
     var volume: Float = 1.0F
     var pitch: Float = 1.0F
-    var scale: Scale? = null
 
-    fun play() {
-        pitch = scale?.float ?: pitch
-        if (location != null) location!!.world.playSound(location!!, sound, soundCategory, volume, pitch)
-        else for (p in players) p.playSound(p.location, sound, soundCategory, volume, pitch)
+    fun play(location: Location) = location.world.playSound(location, sound, soundCategory, volume, pitch)
+
+    fun play(player: Player) = player.playSound(player.location, sound, soundCategory, volume, pitch)
+
+    fun play(players: Collection<Player>) {
+        for (player in players) player.playSound(player.location, sound, soundCategory, volume, pitch)
+    }
+
+    fun setScale(scale: Scale) {
+        pitch = scale.float
     }
 
     fun clone(): PlaySound {
         val playSound = PlaySound()
-        playSound.players = players
-        playSound.location = location
         playSound.sound = sound
         playSound.soundCategory = soundCategory
         playSound.volume = volume
