ec518f64ae987bab877a19eca87955ad028d5982
MineStar <band1019@naver.com>
2021-12-21 01:05:55 +0900

    # 버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "kr.kro.minestar"
-version = "1.5.2"
+version = "1.5.3"
 
 val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\libs")
 //val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.17.1 - vanilla\\plugins")
