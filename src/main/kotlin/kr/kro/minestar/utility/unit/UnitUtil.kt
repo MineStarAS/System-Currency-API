048a6e5554cb11b46317fbd8245cbbe80b5d0008
MineStar <band1019@naver.com>
2021-12-25 09:33:14 +0900

    # Unit 만 적용되게 수정

--- src/main/kotlin/kr/kro/minestar/utility/unit/UnitUtil.kt
+++ src/main/kotlin/kr/kro/minestar/utility/unit/UnitUtil.kt
@@ -1,7 +1,7 @@
 package kr.kro.minestar.utility.unit
 
 class UnitUtil {
-    fun <E, T> E.set(any: T) = any
-    fun <E> E.setTrue() = true
-    fun <E> E.setFalse() = false
+    fun  <T> Unit.set(any: T) = any
+    fun  Unit.setTrue() = true
+    fun  Unit.setFalse() = false
 }
