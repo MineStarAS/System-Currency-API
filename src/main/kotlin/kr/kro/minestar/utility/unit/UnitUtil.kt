400dd74266ffbddb86d5cd81444d1213ab7c3a79
MineStar <band1019@naver.com>
2021-12-25 09:31:05 +0900

    # boolean 은 입력 값 없이 바로 넣어주는 함수 추가

--- src/main/kotlin/kr/kro/minestar/utility/unit/UnitUtil.kt
+++ src/main/kotlin/kr/kro/minestar/utility/unit/UnitUtil.kt
@@ -1,7 +1,7 @@
 package kr.kro.minestar.utility.unit
 
 class UnitUtil {
-    fun <E, T> E.set(any: T): T {
-        return any
-    }
+    fun <E, T> E.set(any: T) = any
+    fun <E> E.setTrue() = true
+    fun <E> E.setFalse() = false
 }
