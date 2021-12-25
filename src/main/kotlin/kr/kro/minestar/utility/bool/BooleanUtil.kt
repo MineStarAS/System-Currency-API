91a88e9a6ed254ee2202ddcb9071a4b4a41be374
MineStar <band1019@naver.com>
2021-12-25 09:23:36 +0900

    # addScript 입력값이 없는 상태 추가

--- src/main/kotlin/kr/kro/minestar/utility/bool/BooleanUtil.kt
+++ src/main/kotlin/kr/kro/minestar/utility/bool/BooleanUtil.kt
@@ -3,3 +3,7 @@ package kr.kro.minestar.utility.bool
 fun Boolean.addScript(script: String): BooleanScript {
     return BooleanScript(this, script)
 }
+
+fun Boolean.addScript(): BooleanScript {
+    return BooleanScript(this, "")
+}
