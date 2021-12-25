ce1101f6a8a1fa38792f5e48841c03a0ae8f2a3e
MineStar <band1019@naver.com>
2021-12-25 09:36:11 +0900

    # 코드 미스 수정

--- src/main/kotlin/kr/kro/minestar/utility/unit/UnitUtil.kt
+++ src/main/kotlin/kr/kro/minestar/utility/unit/UnitUtil.kt
@@ -1,7 +1,5 @@
 package kr.kro.minestar.utility.unit
 
-class UnitUtil {
-    fun  <T> Unit.set(any: T) = any
-    fun  Unit.setTrue() = true
-    fun  Unit.setFalse() = false
-}
+fun <T> Unit.set(any: T) = any
+fun Unit.setTrue() = true
+fun Unit.setFalse() = false
