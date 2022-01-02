6fd3a9d606cfec2d52a43989c4d4278541bdb713
MineStar <band1019@naver.com>
2022-01-02 14:56:28 +0900

    # 필요 없어진 코드 제거

--- src/main/kotlin/kr/kro/minestar/utility/yaml/YamlUtil.kt
+++ src/main/kotlin/kr/kro/minestar/utility/yaml/YamlUtil.kt
@@ -1,8 +1,1 @@
 package kr.kro.minestar.utility.yaml
-
-import org.bukkit.configuration.file.YamlConfiguration
-
-fun YamlConfiguration.clear() {
-    val keys = getKeys(false)
-    for (key in keys) this[key] = null
-}
