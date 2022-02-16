850c693d315269c4321c1b993aea43732e164d64
MineStar <band1019@naver.com>
2022-02-16 11:54:37 +0900

    # 플러그인화

---
+++ src/main/kotlin/kr/kro/minestar/utility/Main.kt
@@ -0,0 +1,8 @@
+package kr.kro.minestar.utility
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main: JavaPlugin() {
+    override fun onEnable() = logger.info("§f[§5Utility-API§f] §aEnable")
+}
