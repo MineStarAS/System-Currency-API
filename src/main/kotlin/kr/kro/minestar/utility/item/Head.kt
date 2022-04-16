9632eefce5ef1449b14675e31e88e66a6fd41f49
MineStar <band1019@naver.com>
2022-04-16 18:50:23 +0900

    # 메소드명 변경

--- src/main/kotlin/kr/kro/minestar/utility/item/Head.kt
+++ src/main/kotlin/kr/kro/minestar/utility/item/Head.kt
@@ -6,6 +6,6 @@ import org.bukkit.Material
 
 object Head {
     private val hdb = HeadDatabaseAPI()
-    fun head(id: Int) = hdb.getItemHead("$id") ?: Material.BARRIER.item().display("§c해당 ID의 머리가 없습니다")
-    fun head(id: Int, material: Material) = hdb.getItemHead("$id") ?: material.item()
+    fun get(id: Int) = hdb.getItemHead("$id") ?: Material.BARRIER.item().display("§c해당 ID의 머리가 없습니다")
+    fun get(id: Int, material: Material) = hdb.getItemHead("$id") ?: material.item()
 }
