f35019444f8d102bc78cbda2b81682c802fe019c
MineStar <band1019@naver.com>
2022-04-24 23:05:15 +0900

    # 코드 수정

--- src/main/kotlin/kr/kro/minestar/utility/item/Head.kt
+++ src/main/kotlin/kr/kro/minestar/utility/item/Head.kt
@@ -5,7 +5,6 @@ import me.arcaniax.hdb.api.HeadDatabaseAPI
 import org.bukkit.Material
 
 object Head {
-    private val hdb = HeadDatabaseAPI()
-    fun get(id: Int) = hdb.getItemHead("$id") ?: Material.BARRIER.item().display("§c해당 ID의 머리가 없습니다")
-    fun get(id: Int, material: Material) = hdb.getItemHead("$id") ?: material.item()
+    fun get(id: Int) = HeadDatabaseAPI().getItemHead("$id") ?: Material.BARRIER.item().display("§c해당 ID의 머리가 없습니다")
+    fun get(id: Int, material: Material) = HeadDatabaseAPI().getItemHead("$id") ?: material.item()
 }
