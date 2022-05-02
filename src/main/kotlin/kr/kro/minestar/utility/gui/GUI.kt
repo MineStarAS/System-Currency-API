b95ecad8be07bf1c1296122412c3642fe597544e
MineStar <band1019@naver.com>
2022-05-03 08:17:02 +0900

    # 인터페이스에서 추상클래스로 변경

--- src/main/kotlin/kr/kro/minestar/utility/gui/GUI.kt
+++ src/main/kotlin/kr/kro/minestar/utility/gui/GUI.kt
@@ -1,36 +1,58 @@
 package kr.kro.minestar.utility.gui
 
-import org.bukkit.Bukkit
+import kr.kro.minestar.utility.event.disable
+import kr.kro.minestar.utility.event.enable
+import kr.kro.minestar.utility.item.Slot
+import kr.kro.minestar.utility.item.SlotKey
+import kr.kro.minestar.utility.item.isSameItem
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
-import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
-interface GUI :Listener {
-    val pl : JavaPlugin
-    val player : Player
-    val gui : Inventory
+abstract class GUI(protected val player: Player) : Listener {
+    protected abstract val pl: JavaPlugin
+    protected abstract val gui: Inventory
 
+    protected abstract fun slots(): Map<out SlotKey, Slot>
 
-    fun openGUI () {
-        Bukkit.getPluginManager().registerEvents(this, pl)
+    protected fun getSlotKey(item: ItemStack, checkAmount: Boolean): SlotKey? {
+        for ((key, slot) in slots()) {
+            if (!slot.item.isSameItem(item)) continue
+            if (checkAmount) if (item.amount != slot.item.amount) continue
+            return key
+        }
+        return null
+    }
+
+    protected fun displaying() {
+        gui.clear()
+        for (slot in slots().values) gui.setItem(slot.get, slot.item)
+    }
+
+    fun openGUI() {
+        enable(pl)
         displaying()
         player.openInventory(gui)
     }
 
     @EventHandler
-    fun clickGUI (e: InventoryClickEvent) {}
+    protected abstract fun clickGUI(e: InventoryClickEvent)
 
-    @EventHandler
-    fun closeGUI (e: InventoryCloseEvent) {
-        if (e.player != player) return
-        if (e.inventory != gui) return
-        HandlerList.unregisterAll(this)
+    protected fun isSameGUI(e: InventoryClickEvent): Boolean {
+        if (e.whoClicked != player) return false
+        if (e.inventory != gui) return false
+        return true
     }
 
-    fun displaying () {}
+    @EventHandler
+    protected fun closeGUI(e: InventoryCloseEvent) {
+        if (e.player != player) return
+        if (e.inventory != gui) return
+        disable()
+    }
 }
