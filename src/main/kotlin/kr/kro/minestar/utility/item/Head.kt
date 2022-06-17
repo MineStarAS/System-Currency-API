4d937acb5c2c9ec9c98d5e8cf3ae5663f8671802
MineStar <band1019@naver.com>
2022-06-18 03:22:05 +0900

    # HeadDataBase 플러그인이 없어도 작동하게 수정

--- src/main/kotlin/kr/kro/minestar/utility/item/Head.kt
+++ src/main/kotlin/kr/kro/minestar/utility/item/Head.kt
@@ -1,10 +1,32 @@
 package kr.kro.minestar.utility.item
 
+import kr.kro.minestar.utility.file.child
 import kr.kro.minestar.utility.material.item
 import me.arcaniax.hdb.api.HeadDatabaseAPI
 import org.bukkit.Material
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
 
-object Head {
-    fun get(id: Int) = HeadDatabaseAPI().getItemHead("$id") ?: Material.BARRIER.item().display("§c해당 ID의 머리가 없습니다")
-    fun get(id: Int, material: Material) = HeadDatabaseAPI().getItemHead("$id") ?: material.item()
+class Head(plugin: JavaPlugin) {
+    val headFile = plugin.dataFolder.child("headItem.yml")
+    fun yaml() = YamlConfiguration.loadConfiguration(headFile)
+
+    val nullItem = Material.BARRIER.item().display("§c해당 ID의 머리가 없습니다")
+
+    fun item(id: Int) = headDataBaseItem(id) ?: nullItem
+
+    fun item(id: Int, material: Material) = headDataBaseItem(id) ?: material.item()
+
+    fun headDataBaseItem(id: Int): ItemStack? {
+        try {
+            val item = HeadDatabaseAPI().getItemHead("$id") ?: return null
+            item.clearDisplay()
+            val yaml = yaml()
+            yaml["$id"] = item
+            return item
+        } catch (_: Exception) {
+            return yaml().getItemStack("$id")
+        }
+    }
 }
