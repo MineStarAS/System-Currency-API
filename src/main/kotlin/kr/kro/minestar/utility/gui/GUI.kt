18a07d104dc515ea886676782695ad8e7baf6f8a
MineStar <band1019@naver.com>
2022-06-18 02:26:14 +0900

    # 불편한 부분 개선

--- src/main/kotlin/kr/kro/minestar/utility/gui/GUI.kt
+++ src/main/kotlin/kr/kro/minestar/utility/gui/GUI.kt
@@ -2,44 +2,23 @@ package kr.kro.minestar.utility.gui
 
 import kr.kro.minestar.utility.event.disable
 import kr.kro.minestar.utility.event.enable
-import kr.kro.minestar.utility.item.Slot
-import kr.kro.minestar.utility.item.SlotKey
-import kr.kro.minestar.utility.item.isSameItem
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.inventory.Inventory
-import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 abstract class GUI(protected val player: Player) : Listener {
     protected abstract val pl: JavaPlugin
     protected abstract val gui: Inventory
 
-    protected abstract fun slots(): Map<out SlotKey, Slot>
-
-    protected fun getSlotKey(item: ItemStack, checkAmount: Boolean): SlotKey? {
-        for ((key, slot) in slots()) {
-            if (!slot.item.isSameItem(item)) continue
-            if (checkAmount) if (item.amount != slot.item.amount) continue
-            return key
-        }
-        return null
-    }
-
-    protected fun baseDisplaying() {
-        gui.clear()
-        for (slot in slots().values) gui.setItem(slot.get, slot.item)
-        displaying()
-    }
-
-    protected abstract fun displaying()
+    protected open fun displaying() {}
 
     fun openGUI() {
         enable(pl)
-        baseDisplaying()
+        displaying()
         player.openInventory(gui)
     }
 
