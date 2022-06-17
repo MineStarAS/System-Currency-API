95059b0fe530a717d2fa88bb483e6492bf8fdc90
MineStar <band1019@naver.com>
2022-06-18 02:26:47 +0900

    # Slot 을 객체가 아닌 enum class 방식으로 사용 할 수 있게 변경

--- src/main/kotlin/kr/kro/minestar/utility/item/Slot.kt
+++ src/main/kotlin/kr/kro/minestar/utility/item/Slot.kt
@@ -1,25 +1,43 @@
 package kr.kro.minestar.utility.item
 
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 
 /**
- * 인벤토리의 setItem 함수를 편하게 사용하기 위해 만든 클래스 입니다.
- * *아래 내용은 기본적인 사용법 예시 입니다.
+ *   사용 예시
  *
- *      val slot = listof(
- *           Slot(0, 0, Metarial.STONE.item()),
- *           Slot(0, 1, Metarial.COBBLESTONE.item()),
- *           Slot(0, 2, Metarial.DEEPSLATE.item()),
- *           Slot(0, 3, Metarial.COBBLED_DEEPSLATE.item()),
+ *    enum class TestSlot(override val line: Int,
+ *    override val number: Int, override val item: ItemStack): Slot {
  *
- *           Slot(1, 0, Metarial.RAW_COPPER.item()),
- *           Slot(1, 1, Metarial.RAW_IRON.item()),
- *           Slot(1, 2, Metarial.RAW_GOLD.item()),
- *           )
- * val gui = Bukkit.createInventory(null, 9 * 2, "예제 인벤토리").also {
- * for (slot in slots) it.setItem(slot.get, slot.item)
- * }
+ *    T1(0, 0, Material.STONE.item()),
+ *    T2(0, 1, Material.STONE.item()),
+ *    T3(0, 2, Material.STONE.item()),
+ *    ;
+ *
+ *    override fun enumValues() = values()
+ *    }
+ *
+ *
+ *
+ *
+ *
  */
-class Slot(line: Int, number: Int, val item: ItemStack) {
-    val get = line * 9 + number
+interface Slot {
+    val item: ItemStack
+
+    val line: Int
+    val number: Int
+
+    fun getIndex() = line * 9 + number
+
+    fun enumValues(): Array<out Slot>
+
+    fun setItems(inventory: Inventory) {
+        for (slot in enumValues()) inventory.setItem(getIndex(), slot.item)
+    }
+
+    fun getSlot(item: ItemStack): Slot? {
+        for (slot in enumValues()) if (slot.item.isSameItem(item)) return slot
+        return null
+    }
 }
--- src/main/kotlin/kr/kro/minestar/utility/item/SlotKey.kt
+++
@@ -1,7 +0,0 @@
-package kr.kro.minestar.utility.item
-
-interface SlotKey
-
-/**
- * GUI class 에서 Slots 에 Key 로 사용 될 enum class 에 상속되어 사용 되는 interface 입니다.
- */
