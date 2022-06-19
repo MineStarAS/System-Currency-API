b7c7ecbf672225b4abb0f2881097b23c026ba62e
MineStar <band1019@naver.com>
2022-06-20 07:17:47 +0900

    # 프로젝트 버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.0"
     id("org.jetbrains.dokka") version "1.5.0"
     `maven-publish`
