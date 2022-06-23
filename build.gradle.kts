0845f45f039dfeaeca0ac6ff7db1671305bcc15f
MineStar <band1019@naver.com>
2022-06-24 01:27:51 +0900

    # Command 유틸 코드 대량 개편으로 인한 프로젝트 버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.0"
     id("com.github.johnrengelman.shadow") version "7.1.0"
     id("org.jetbrains.dokka") version "1.5.0"
     `maven-publish`
 }
 
 group = "kr.kro.minestar"
-version = "1.1.2"
+version = "1.2.0"
 
 val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\libs")
 
