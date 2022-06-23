29ef1e96b96345b8273a8c7719c21ae6ee61df3c
MineStar <band1019@naver.com>
2022-06-24 01:25:49 +0900

    # 플러그인 접두사 추가

--- src/main/kotlin/kr/kro/minestar/utility/main/FunctionalJavaPlugin.kt
+++ src/main/kotlin/kr/kro/minestar/utility/main/FunctionalJavaPlugin.kt
@@ -3,6 +3,5 @@ package kr.kro.minestar.utility.main
 import org.bukkit.plugin.java.JavaPlugin
 
 abstract class FunctionalJavaPlugin : JavaPlugin() {
-    protected var privatePrefix: String = ""
-    val prefix = privatePrefix
+    lateinit var prefix : String
 }
